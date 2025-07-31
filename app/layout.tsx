1f3f3edbfc0b58651494761bbd6c1cf35a8fbb3a
Hiroaki Ogasawara <13391129+xhiroga@users.noreply.github.com>
2025-07-31 21:24:15 +0900

    feat: Add Google Analytics component for tracking page views

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import GoogleAnalytics from "../components/GoogleAnalytics";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -81,6 +82,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <GoogleAnalytics />
         {children}
       </body>
     </html>
